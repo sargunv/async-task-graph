02a1651fd83e8d8ce4486094a2456f705c748ed7
Sargun Vohra <sargun.vohra@gmail.com>
2023-02-14 15:18:52 -0800

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import { Task, TaskId, UnknownWorkflowDefinition } from "./core-types.js"
 
+export type {
+  Task,
+  TaskFor,
+  TaskId,
+  TaskResult,
+  TaskRunContext,
+  UnknownWorkflowDefinition,
+  Workflow,
+} from "./core-types.js"
+export { WorkflowError } from "./errors.js"
+export type { EventEmitter, EventSink, EventSource } from "./event-emitter.js"
+export type {
+  TaskEvents,
+  TaskFinishArgs,
+  TaskSkipArgs,
+  TaskStartArgs,
+  TaskThrowArgs,
+  WorkflowEvents,
+  WorkflowFinishArgs,
+  WorkflowStartArgs,
+} from "./event-types.js"
 export {
   concurrentExecutor,
   serialExecutor,
   workflowBuilder,
   type WorkflowExecutor,
 } from "./workflow.js"
+
+// type inference helpers below
 
 export type WorkflowDefinition<W extends UnknownWorkflowDefinition> = W
 
